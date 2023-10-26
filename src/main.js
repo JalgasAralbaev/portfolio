39724ccfc928fc328481c6b3d75398774e4a8492
Jalgas <65507582+JalgasAralbaev@users.noreply.github.com>
2023-10-26 08:40:50 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,200 +1,192 @@
-const input = document.querySelector("input");
-const letters = Array.from(document.querySelectorAll("[data-key]"));
-const specs = Array.from(document.querySelectorAll("[data-spec]"));
-const textExample = document.getElementById('textExample');
-
-const text = `В начале 13 в. в Центральной Азии возникло новое государство - Монгольская империя. Объединение монгольских племен в немалой степени было вызвано изменением климатических условий местности, где проживали монголы. 11 и 12 вв. были благоприятными для монголов. Длительный период влажных лет в восточной степи привел к тому, что умножились стада, а, следовательно, одна и та же территория могла прокормить больше людей. Произошло увеличение населения в Монголии. Однако в конце 12 в. климат стал постепенно меняться в сторону ухудшения, стал более засушливым. Кочевое скотоводство стало малопродуктивным, в степи стало много избыточного населения. Началась обычная в таких условиях борьба с соседями за пастбища, а также вторжения на земли соседей-земледельцев.
+const text = `В начале 13 в.в Центральной Азии возникло новое государство - Монгольская империя. Объединение монгольских племен в немалой степени было вызвано изменением климатических условий местности, где проживали монголы. 11 и 12 вв. были благоприятными для монголов. Длительный период влажных лет в восточной степи привел к тому, что умножились стада, а, следовательно, одна и та же территория могла прокормить больше людей. Произошло увеличение населения в Монголии. Однако в конце 12 в. климат стал постепенно меняться в сторону ухудшения, стал более засушливым. Кочевое скотоводство стало малопродуктивным, в степи стало много избыточного населения. Началась обычная в таких условиях борьба с соседями за пастбища, а также вторжения на земли соседей-земледельцев.
 Чингисхан.Огромную роль в создании Монгольской империи сыграл Темуджин. Железная воля, властолюбие и целеустремленность позволили Темуджину собрать монголов в единое политическое целое и спаять общество на основе т. н. закона Чингисхана - Яссы. На съезде монгольской знати (курултае) в 1206 г. создатель империи Темуджин (Темучин) был провозглашен великим ханом всех монголов и принял имя Чингисхан. Монгольская империя оказалось исключительно боеспособной и жизнеспособной. Чингисхан силой объединил под своей рукой всех монголов, некоторые соседние племена и на основе родового признака создал войско, которому в XII - XIII вв., в среднеазиатских государствах, на Руси и в Европе равных не было. В последующие годы монгольские отряды подчинили себе окрестные народы, беспощадно уничтожая всех непокорных. В середине 20-х гг. XIII в. в состав монгольского государства вошли Северный Китай, Средняя Азия, Северная Индия.
 Рядовой единицей монгольского войска была десятка - семья, ближайшие родственники одной юрты, одного аила. Потом следовала сотня, в нее входили люди одного рода. Тысяча могла объединять два или три аила, далее шла тьма - десятитысячный отряд. В войске Чингисхана действовала Ясса: если в бою кто-то из десятки побежит от врага, то казнили всю десятку; если в сотне побежит десятка, то казнили всю сотню, если побежит сотня и откроет брешь врагу, то казнили всю тысячу. Основную ударную силу монголов составляла конница. Каждый воин имел два-три лука, веревочный аркан, топор, хорошо владел саблей. Лошадь воина покрывалась шкурами для защиты. Голову, шею и грудь воина защищали железный или медный шлем, панцирь из кожи. Монгольская конница на своих низкорослых выносливых конях могла проходить в сутки до 80 км, а с обозами, стенобитными, огнеметными орудиями - до 10 км.
 К 1211 г. монголы завоевали землю бурят, якутов, киргизов и уйгуров, т.е. подчинили себе практически все основные племена и народы Сибири, обложив их данью. В 1211 г. Чингисхан приступил к завоеванию северного Китая, которое было завершено лишь к 1234 г. Монголы в процессе завоевания заимствовали у китайцев различную военную технику, а также научились осаждать крепости при помощи стенобитных и осадных машин. Китайцы передали монголам и важную науку дипломатии. Монголы одними из первых стали придерживаться принципа неприкосновенности послов, беспощадно карая народы за нарушение этого принципа.Схватка грузин с монголо-татарами.
 В 1218 г. татаро-монголы покорили всю Корею. После этого Чингисхан устремил свой взгляд на богатейшие государства Средней Азии. Цель Чингисхана - покорение городов Бухары, Самарканда, Мерва, Ургенча и других. Всё завоевание было совершено за 3 года - 1219-1221 гг. Хорезмшах Мухаммед недооценил силу Чингисхана, вследствие чего вынужден был спастись бегством. В погоню было отправлено несколько туменов под руководством военноначальников Джебе и Субэдея. Монголы огнем и мечом прошли по Северному Ирану, вышли на Кавказ, разрушили несколько древних и богатых городов, разбили грузинские войска, проникли через Ширванское ущелье на Северный Кавказ и столкнулись с половцами. В это время состоялась первая встреча русских воинов с новым врагом и произошла (1223 г.). К концу жизни Чингисхана (1227г.) монголы подчинили огромные территории от Тихого океана на востоке до Каспийского моря на западе.
 Причины успехов монголов состояли в следующем: Первая причина - Китай, Средняя Азия и Иран переживали в то время период феодальной раздробленности и не смогли организовать коллективного отпора монголам. Вторая причина - это великолепная военная и политическая подготовка вторжения. Проведя разведку, монголы стравливали народы и раздували междоусобицы. Если удавалось, то монголы занимали ключевые военные посты в армии намеченной жертвы (Хорезм). Тактика завоеваний была отточена до совершенства. По возможности избегая фронтальных сражений, кочевники разбивали противника по частям, предварительно измотав его непрерывными стычками и налетами.
 Еще при жизни Чингисхан разделил огромную империю между сыновьями на улусы, которые были в составе единого государства еще 40 лет после его смерти (1227 г.). Улус Угедэя - собственно Монголия и Северный Китай, улус Чагатая - Средняя Азия, улус Джучи - пространства к западу и югу от Иртыша до Уральских гор, Аральского и Каспийского морей. В 40-х гг. XIII в. выделился еще один улус, охватывающий часть Ирана и Закавказье, который был отдан внуку Чингисхана Хулагу. Далее процесс распада великой империи ускорился. В начале XIV в. улус Джучи распался на Синюю и Белую Орду. Впоследствии за Белой Ордой, располагавшейся в бассейне рек Волги и Дона, в Крыму и на Северном Кавказе, закрепилось название Золотой Орды.`;
-
+const backgrounds = ['three', 'two', 'one', 'zero'];
+const stop = document.querySelector('.timer');
 const party = createParty(text);
-
-
 init();
-press()
-
+viewUpdate()
 function init() {
-    document.body.addEventListener("keydown", keydownHandler);
-    document.body.addEventListener("keyup", keyupHandler);
+	addListener();
 }
 
-function keydownHandler(event) {
-    event.preventDefault();
-
-    const letter = letters.find((x) => x.dataset.key.includes(event.key));
-
-    if (letter) {
-        letter.classList.add("pressed");
-        press(event.key);
-        return;
-    }
-
-    let key = event.key.toLowerCase();
-
-    if (key === " ") {
-        key = "space";
-        press(' ')
-    }
-
-    const ownSpecs = specs.filter((x) => x.dataset.spec === key);
-
-    if (ownSpecs.length) {
-        ownSpecs.forEach((spec) => spec.classList.add("pressed"));
-        return;
-    }
-
-    console.warn("Не известный вид клавиши.", event);
+function addListener() {
+	document.body.addEventListener('keydown', handlerKeydown);
+	document.body.addEventListener('keyup', handlerKeyup);
 }
 
-function keyupHandler(event) {
-    event.preventDefault();
+function removeListener() {
+	document.body.removeEventListener('keydown', handlerKeydown);
+	document.body.removeEventListener('keyup', handlerKeyup);
+}
 
-    const letter = letters.find((x) => x.dataset.key.includes(event.key));
+function handlerKeydown(e) {
 
-    if (letter) {
-        letter.classList.remove("pressed");
-        return;
-    }
+	if (e.keyCode !== 9 && e.keyCode !== 18 && e.keyCode !== 16 && e.keyCode !== 8 && e.keyCode !== 13) {
+		press(e.key);
+	}
 
-    let key = event.key.toLowerCase();
 
-    if (key === " ") {
-        key = "space";
-    }
+}
 
-    const ownSpecs = specs.filter((x) => x.dataset.spec === key);
+function handlerKeyup(e) {
 
-    if (ownSpecs.length) {
-        ownSpecs.forEach((spec) => spec.classList.remove("pressed"));
-        return 1;
-    }
 }
 
 function createParty(text) {
-    const party = {
-        text,
-        strings: [],
-        maxStringLength: 50,
-        maxShowStrings: 4,
-        currentStringIndex: 0,
-        currentPressedIndex: 0,
-        currentPressedString: 1,
-        errors: [],
-    };
+	const party = {
+		text,
+		strings: [],
+		maxStringLength: 50,
+		maxShowStrings: 5,
+		currentStringIndex: 0,
+		currentPressedIndex: 0,
+		currentPressedError: undefined,
+		error: 5,
+	};
 
-    party.text = party.text.replace(/\n/g, "\n ");
-    const words = party.text.split(" ");
+	party.text = party.text.replace(/\n/g, "\n ");
+	const words = party.text.split(" ");
 
-    let string = [];
-    for (const word of words) {
-        const newStringLength =
-            [...string, word].join(" ").length + !word.includes("\n");
+	let string = [];
+	for (const word of words) {
+		const newStringLength =
+			[...string, word].join(" ").length + !word.includes("\n");
 
-        if (newStringLength > party.maxStringLength) {
-            party.strings.push(string.join(" ") + " ");
-            string = [];
-        }
+		if (newStringLength > party.maxStringLength) {
+			party.strings.push(string.join(" ") + " ");
+			string = [];
+		}
 
-        string.push(word);
+		string.push(word);
 
-        if (word.includes("\n")) {
-            party.strings.push(string.join(" "));
-            string = [];
-        }
-    }
+		if (word.includes("\n")) {
+			party.strings.push(string.join(" "));
+			string = [];
+		}
+	}
 
-    if (string.length) {
-        party.strings.push(string.join(" "));
-    }
+	if (string.length) {
+		party.strings.push(string.join(" "));
+	}
 
-    return party;
+	return party;
 }
-
 function press(letter) {
-    const string = party.strings[party.currentStringIndex];
-    const mustLetter = string[party.currentPressedIndex];
+	const string = party.strings[party.currentStringIndex];
+	const mustLetter = string[party.currentPressedIndex];
 
 
-    if (letter === mustLetter) {
-        party.currentPressedIndex++;
-        party.currentPressedString++;
+	if (mustLetter === letter) {
+		party.currentPressedIndex++;
+		party.currentPressedError = undefined;
+		if ((string.length <= party.currentPressedIndex)) {
+			party.currentPressedIndex = 0;
+			party.currentStringIndex++;
+		}
+	} else if (mustLetter !== letter) {
+		party.currentPressedError = letter;
 
-        if ((string.length <= party.currentPressedIndex)) {
-            party.currentPressedIndex = 0;
-            party.currentPressedString = 1;
-            party.currentStringIndex++;
+		setTimeout(() => {
+			removeListener()
+			stop.classList.add(backgrounds[0]);
 
-        }
-    } else if (!party.errors.includes(mustLetter)) {
-        party.errors.push(mustLetter);
-    }
-    if (letter !== mustLetter) {
-        party.currentPressedString = 1;
-        party.currentPressedIndex = 0;
-    }
-    viewUpdate(mustLetter);
+			setTimeout(() => {
+				stop.classList.remove(backgrounds[0]);
+				stop.classList.add(backgrounds[1]);
+			}, 1000)
+			setTimeout(() => {
+				stop.classList.remove(backgrounds[1]);
+				stop.classList.add(backgrounds[2]);
+			}, 2000)
+			setTimeout(() => {
+				stop.classList.remove(backgrounds[2]);
+				stop.classList.add(backgrounds[3]);
+			}, 3000)
+			setTimeout(() => {
+				stop.classList.remove(backgrounds[3]);
+				party.currentPressedIndex = 0;
+				party.error++;
+				party.currentPressedError = undefined;
+				addListener();
+				viewUpdate();
+			}, 4000)
+		}, 0)
+	}
+
+	viewUpdate();
 
 }
+function viewUpdate() {
 
-function viewUpdate(mustLetter) {
-    const string = party.strings[party.currentStringIndex];
+	const textExample = document.querySelector('#textExample');
+	const string = party.strings[party.currentStringIndex];
 
+	const showedString = party.strings.slice(
+		party.currentStringIndex, party.currentStringIndex + party.maxShowStrings
+	);
 
-    const showedString = party.strings.slice(
-        party.currentStringIndex,
-        party.currentStringIndex + party.maxShowStrings
-    );
+	const div = document.createElement('div');
+	const firstLine = document.createElement('div');
+	const currentPressedString = document.createElement('span');
+	const span = document.createElement('span');
+	currentPressedString.classList.add('nextKey');
+	firstLine.classList.add('row');
+	div.append(firstLine);
+	const done = document.createElement('span');
+	done.classList.add('done');
+	let words = [...string];
 
-    const div = document.createElement('div');
-    const firstLine = document.createElement('div');
-    const nextKey = document.createElement('span');
-    const done = document.createElement('span');
+	if (party.currentPressedError !== undefined) {
 
-    nextKey.classList.add('nextKey');
-    firstLine.classList.add('line');
-    div.append(firstLine);
-    done.classList.add('done');
-    done.textContent = string.slice(0, party.currentPressedIndex);
-
-    firstLine.append(
-        done,
-        ...string
-            .slice(party.currentPressedIndex)
-            .split('')
-            .map((letter, index, array) => {
-                nextKey.textContent = `${array[0]}`;
-                return nextKey;
-            }),
-        ...string
-            .slice(party.currentPressedString)
-            .split('')
-            .map((letter) => {
-                return `${letter}`;
-            })
-    );
+		if (party.currentPressedError === ' ') {
+			party.currentPressedError = '&nbsp;'
+		}
 
 
-    for (let i = 1; i < showedString.length; i++) {
+		currentPressedString.innerHTML = words.splice(party.currentPressedIndex, 1, party.currentPressedError);
+		currentPressedString.classList.add('error');
 
-        const line = document.createElement('div');
-        line.classList.add('line');
 
-        div.append(line);
+	}
+	currentPressedString.innerHTML = words.splice(party.currentPressedIndex, 1)
+	let correct = [...words.splice(0, party.currentPressedIndex)]
 
-        line.append(
-            ...showedString[i].split('').map((letter) => {
+	done.innerHTML = correct.join('');
+	span.innerHTML = words.join('')
 
-                return letter;
-            })
-        )
-    }
 
-    textExample.innerHTML = '';
-    textExample.append(div);
 
-    input.value = string.slice(0, party.currentPressedIndex);
+
+
+
+
+	firstLine.append(
+		done, currentPressedString, span
+	);
+
+
+
+
+
+	for (let i = 1; i< showedString.length; i++) {
+		const line = document.createElement('div');
+		line.classList.add('row');
+		div.append(line);
+
+		line.append(
+			...showedString[i].split('').map((letter) => {
+
+				return `${letter}`;
+			})
+		)
+	}
+
+	textExample.innerHTML = '';
+	textExample.append(div);
 }
+
